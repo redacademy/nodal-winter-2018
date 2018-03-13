4545562c8387b14699f4d5485556e6e4897f7279
qingxin-yuan <qingxin.yn@gmail.com>
2018-03-13 10:45:01 -0700

    Add firebase config in the config folder.

---
+++ js/config/firebaseConfig.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase';
+
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyBiRnYQX_1upYOMLzkT4yBmpobc0RjSHJw",
+  authDomain: "nodal-e60d0.firebaseapp.com",
+  databaseURL: "https://nodal-e60d0.firebaseio.com",
+  projectId: "nodal-e60d0",
+  storageBucket: "nodal-e60d0.appspot.com",
+  messagingSenderId: "148407972647"
+};
+
+const firebaseApp = firebase.initializeApp(config);
+const firebaseAuth = firebase.auth();
+// Initialize Cloud Firestore through Firebase
+const firebaseDB = firebase.firestore();
+
+
+export { firebaseApp, firebaseAuth, firebaseDB };
+
