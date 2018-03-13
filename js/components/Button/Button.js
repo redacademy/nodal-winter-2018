1d9224b687f6cdecb6993acb8983d4ea1133f44e
qingxin-yuan <qingxin.yn@gmail.com>
2018-03-13 11:54:48 -0700

    Add firebase create user, store user data in firestore, sign in, sign out helper functions. Also clean up button component.

--- js/components/Button/Button.js
+++ js/components/Button/Button.js
@@ -1,27 +1,25 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import PropTypes from "prop-types";
 
-const Button = ({ color, text, func}) => (
-  <TouchableOpacity style={[styles.background, { backgroundColor: color }]} onPress={func} >
-    <Text style={styles.text}>{text}</Text>
-  </TouchableOpacity>
-);
+import { styles } from "./styles";
+
+// TODO: Add config styles once it's pushed up.
+const Button = ({ color, text, func }) => {
+  return (
+    <TouchableOpacity
+      style={[styles.background, { backgroundColor: color }]}
+      onPress={func}
+    >
+      <Text style={styles.text}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
 
 export default Button;
 
-
-// TODO: Add config styles once it's pushed up.
-const styles = StyleSheet.create({
-  background: {
-    width: 275,
-    height: 50,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 25
-  },
-  text: {
-    color: "white",
-    fontSize: 20 
-  }
-});
+Button.propTypes = {
+  color: PropTypes.string,
+  text: PropTypes.string,
+  func: PropTypes.func,
+};
---
+++ js/helpers/firebaseAuth.js
@@ -0,0 +1,14 @@
+import {firebaseAuth, firebaseDB} from '../config/firebaseConfig';
+
+export const createUserInAuthAndDB = (fullname, email, password) =>{
+  console.log('creating account...')
+  return firebaseAuth.createUserWithEmailAndPassword(email, password).then(authUser=>firebaseDB.collection("users").add({
+    fullname,email, password
+  })).catch(err=>console.log("something went wrong with creating account: ", err.message))
+}
+
+export const signIn = (email, password) => {firebaseAuth.signInWithEmailAndPassword(email, password).then(currUser=>console.log(currUser.uid)).catch(err=>console.log("something went wrong with signIn: ", err.message))
+
+}
+
+export const signOut = () => firebaseAuth.signOut().then(()=>console.log("sign out success")).catch((err)=>console.log("something went wrong with sign out"));
