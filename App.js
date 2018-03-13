3bba8a2bf291a1b32332eeca9a2627304671d989
NCMoseley <ncmoseley@gmail.com>
2018-03-12 21:21:10 -0700

    Add CSS conventions, create Profile UI boilerplate

--- App.js
+++ App.js
@@ -6,7 +6,11 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Button from "./js/components/Button";
-import { createUserInAuthAndDB, signIn, signOut } from "./js/helpers/firebaseAuth";
+import {
+  createUserInAuthAndDB,
+  signIn,
+  signOut
+} from "./js/helpers/firebaseAuth";
 
 export default class App extends Component {
   render() {
@@ -15,19 +19,14 @@ export default class App extends Component {
         <Button
           color={"coral"}
           text={"Sign Up"}
-          func={()=>createUserInAuthAndDB("jon", "jon@nodal.com", "password")}
+          func={() => createUserInAuthAndDB("jon", "jon@nodal.com", "password")}
         />
         <Button
           color={"coral"}
           text={"Sign In"}
-          func={()=>signIn("jon@nodal.com", "password")}
+          func={() => signIn("jon@nodal.com", "password")}
         />
-        <Button
-          color={"coral"}
-          text={"Sign Out"}
-          func={()=>signOut()}
-        />
-
+        <Button color={"coral"} text={"Sign Out"} func={() => signOut()} />
       </View>
     );
   }
---
+++ js/screens/Profile/ProfileContainer.js
@@ -0,0 +1,42 @@
+/* global require */ // This is to avoid a linting error with 'require'
+
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Profile from "./Profile";
+import { Image, View } from "react-native";
+import { styles } from "./styles";
+
+class ProfileContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: ""
+    };
+  }
+  static route = {
+    navigationBar: {
+      title: "Profile",
+      tintColor: "white",
+      titleStyle: { fontFamily: "mission-gothic-bold" }
+    }
+  };
+
+  render() {
+    const { loading } = this.props;
+
+    return loading ? (
+      <View style={styles.loadinggif}>
+        {/* <Image source={require("../../assets/images/loading_blue.gif")} /> */}
+        {/* Placeholder Image */}
+      </View>
+    ) : (
+      <Profile user={this.state.user} />
+    );
+  }
+}
+
+ProfileContainer.propTypes = {
+  loading: PropTypes.bool.isRequired
+};
+
+export default ProfileContainer;
