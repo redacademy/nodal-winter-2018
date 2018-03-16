a7a79c98579fdbae85b92b3560d6aec31c2bdbb1
Ian <ianhitchcock@outlook.com>
2018-03-16 13:37:20 -0700

    Fix style config file.

--- js/config/styles.js
+++ js/config/styles.js
@@ -12,21 +12,20 @@ export const colors = {
   cornflowerBlue: "#6067EC",
   borealisGreen: "#4ABB8B",
   transluscentGrey: "rgba(0,0,0,0.4)",
-  borderGrey: "rgba(158, 158, 158, 1)",
   transluscentWhite: "rgba(255,255,255,0.8)"
 };
 
 export const typography = {
   fontMainLight: "MissionGothic-Light",
-  fontMain: "Mission Gothic",
+  fontMain: "MissionGothic-Regular",
   fontBold: "MissionGothic-Bold",
-  fontBlack: "mission-gothic-black",
-  fontThin: "mission-gothic-thin",
-  fontMainLightItalic: "mission-gothic-light-italic",
-  fontMainItalic: "mission-gothic-regular-italic",
-  fontBoldItalic: "mission-gothic-bold-italic",
-  fontBlackItalic: "mission-gothic-black-italic",
-  fontThinitalic: "mission-gothic-thin-italic"
+  fontBlack: "MissionGothic-Black",
+  fontThin: "MissionGothic-Thin",
+  fontMainLightItalic: "MissionGothic-LightItalic",
+  fontMainItalic: "MissionGothic-RegularItalic",
+  fontBoldItalic: "MissionGothic-BoldItalic",
+  fontBlackItalic: "MissionGothic-BlackItalic",
+  fontThinitalic: "MissionGothic-ThinItalic"
 };
 
 export const containerMargin = {
