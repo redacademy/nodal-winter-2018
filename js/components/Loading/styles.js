1077ed0248b097b6ed9092a5dbccb74c6a98f3e1
Ian <ianhitchcock@outlook.com>
2018-03-16 19:43:16 -0700

    Build Loading Component

---
+++ js/components/Loading/styles.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  dot: {
+    height: 10,
+    width: 10,
+    position: "absolute"
+  },
+  wrapper: {
+    width: 44.6,
+    height: 50
+  },
+  container: {
+    width: "100%",
+    alignItems: "center"
+  },
+  dot0: {
+    top: 30,
+    left: 34.6
+  },
+  dot1: {
+    top: 40,
+    left: 17.3
+  },
+  dot2: {
+    top: 30,
+    left: 0
+  },
+  dot3: {
+    top: 10,
+    left: 0
+  },
+  dot4: {
+    top: 0,
+    left: 17.3
+  },
+  dot5: {
+    top: 10,
+    left: 34.6
+  }
+});
