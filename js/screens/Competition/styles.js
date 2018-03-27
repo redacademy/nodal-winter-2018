9f2a53d69b9abf291a6dd13ffa035f9a8f336e97
qingxin-yuan <qingxin.yn@gmail.com>
2018-03-27 11:15:27 -0700

    Modify styles for iPhone 6 as well as SE

--- js/screens/Competition/styles.js
+++ js/screens/Competition/styles.js
@@ -1,63 +1,71 @@
-import {StyleSheet} from 'react-native';
+import { Dimensions, StyleSheet } from "react-native";
 
-import {colors, typography} from '../../config/styles';
+import { colors, typography } from "../../config/styles";
+
+const { width } = Dimensions.get("window");
 
 export const styles = StyleSheet.create({
   title: {
     fontFamily: typography.fontMain,
     fontSize: 20,
-    textAlign: 'center',
-    paddingVertical: 5,
+    textAlign: "center"
   },
   subtitle: {
     fontFamily: typography.fontMainLight,
-    textAlign:'center',
+    textAlign: "center",
     marginBottom: 5,
+    fontSize: width > 320 ? 16 : 14
   },
   hero: {
-    width: "100%", 
+    width: "100%",
     height: 200,
+    marginBottom: 10
   },
-  iconTextWrapper:{
-    flexDirection: 'row',
-    marginLeft: 20,
+  iconTextWrapper: {
+    flexDirection: "row",
+    marginLeft: width > 320 ? 40 : 30,
     marginVertical: 10,
+    alignItems: "center"
   },
-  icon:{
+  icon: {
     width: 25,
     height: 25,
-    marginRight: 10,
+    marginRight: 15
   },
   competitionDesText: {
     fontFamily: typography.fontMainLight,
+    fontSize: width > 320 ? 16 : 14
   },
-  blurbWrapper:{
+  blurbWrapper: {
     marginHorizontal: 20,
     marginVertical: 10
   },
-  blurb:{
+  blurb: {
     fontFamily: typography.fontMainLight,
+    fontSize: width > 320? 16 : 14
   },
-  secondaryTitle:{
+  secondaryTitle: {
     fontFamily: typography.fontBold,
+    fontSize: width > 320 ? 16 : 14,
     marginLeft: 20,
-    marginVertical: 10,
+    marginVertical: 10
   },
-  imageGallery:{
+  imageGallery: {
     marginHorizontal: 20,
-    marginVertical: 10,
+    marginVertical: 10
   },
-  singleImage:{
-    width: 100, 
+  singleImage: {
+    width: 100,
     height: 100,
-    marginRight: 10,
+    marginRight: 10
   },
-  all:{
-    paddingVertical: 10,
+  all: {
+    paddingTop: 5,
+    paddingBottom: 10,
     backgroundColor: colors.white
   },
-  readMore:{
+  readMore: {
     fontFamily: typography.fontMainLight,
     color: colors.borealisGreen
   }
-})
+});
